af50d7494bc549a7a7b2856df02afcd05249a13b
Nicholas Edson <nicedson23@gmail.com>
2023-09-07 20:50:10 +0700

    FE - Update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Task from "./components/Task";
+import TaskList from "./components/TaskList";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const [trigger, setTrigger] = useState(false);
   return (
     <div className="App">
       <h1> Task Management App</h1>
-      <Task />
+      <ToastContainer />
+      <Task setTrigger={setTrigger} />
+      <TaskList trigger={trigger} />
     </div>
   );
 }
